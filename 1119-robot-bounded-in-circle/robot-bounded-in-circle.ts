cb3383a873d6b4b4d2fb5de084cb80fb01570119
Aleena Joseph <115602866+aleena-joseph-86@users.noreply.github.com>
2025-09-02 23:47:49 +0530

    Time: 0 ms (100.00%) | Memory: 58.7 MB (18.75%) - LeetSync

--- 1119-robot-bounded-in-circle/robot-bounded-in-circle.ts
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.ts
@@ -5,7 +5,7 @@ function isRobotBounded(instructions: string): boolean {
     for(const instruction of instructions) {
         if (instruction === "G") [x, y]= [x + dirX, y + dirY];
         else if (instruction === "L") [dirX, dirY] =[-1*dirY, dirX];
-        else [dirX, dirY] =[dirY, -1*dirX];
+        else [dirX, dirY] = [dirY, -1*dirX];
     }
    return (x === 0 && y === 0) || (dirX !== 0 || dirY !== 1);
 };
