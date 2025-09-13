d1f7d1e26433048f45dbf7c90b4f87da4e2fbbd6
Aleena Joseph <115602866+aleena-joseph-86@users.noreply.github.com>
2025-09-13 16:26:35 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (79.05%) - LeetSync

---
+++ 169-majority-element/majority-element.ts
@@ -0,0 +1,14 @@
+function majorityElement(nums: number[]): number {
+    const n = nums.length;
+    let count = 0;
+    let element = nums[0];
+    for(let i = 0; i < n; i++) {
+        if(count===0){
+            element = nums[i];
+            count = 0;
+        }
+        if(nums[i]===element) count++;
+        else count--;
+    }
+    return element
+};
