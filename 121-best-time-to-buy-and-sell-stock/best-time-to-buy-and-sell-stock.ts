9b9d67b80f96d9f21e4072864eafc1c039b381ad
Aleena Joseph <115602866+aleena-joseph-86@users.noreply.github.com>
2025-09-21 00:47:59 +0530

    Time: 3 ms (56.25%) | Memory: 66.3 MB (29.67%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,15 @@
+function maxProfit(prices: number[]): number {
+    let max_profit = 0;
+    const n = prices.length;
+    let sell = prices[n-1];
+    for(let i = n-2; i >= 0; i--) {
+        let profit = 0;
+        if(prices[i]<sell) {
+            profit = sell - prices[i];
+        }
+       sell = Math.max(sell, prices[i]);
+       max_profit = Math.max(max_profit, profit);
+    }
+    
+    return max_profit;
+};
