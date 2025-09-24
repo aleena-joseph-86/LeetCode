1779538f8887cd2d17501f863e1788619c36c671
Aleena Joseph <115602866+aleena-joseph-86@users.noreply.github.com>
2025-09-24 21:02:11 +0530

    Time: 0 ms (100.00%) | Memory: 56.1 MB (19.49%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.ts
@@ -0,0 +1,15 @@
+function searchInsert(nums: number[], target: number): number {
+    let low = 0;
+    let high = nums.length - 1;
+    let answer = nums.length;
+    while(low<=high) {
+        const mid = Math.floor((low+high)/2);
+        if(nums[mid] === target) return mid;
+        else if(nums[mid]>=target) {
+            answer = Math.min(answer, mid);
+            high = mid - 1;
+        }
+       else low = mid + 1;
+    }
+    return answer;
+};
